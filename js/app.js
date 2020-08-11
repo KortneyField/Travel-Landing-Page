d125b42fdddb08b3a67da173f2d9a6e8e3a23c3c
KortneyField <kortneyfield@gmail.com>
2020-08-11 14:00:31 -0500

    update visited tabs nav

--- js/app.js
+++ js/app.js
@@ -69,3 +69,19 @@ buildingNav();
 // Set sections as active
 
 
+
+
+//SCROLL TO TOP BUTTON. 
+backToTopButton = document.getElementById("scrollTopButton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+function backToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
