459bb8626102805a0d6ce505390a9834b12e0a0a
KortneyField <kortneyfield@gmail.com>
2020-08-17 13:52:00 -0500

    added comments

--- js/app.js
+++ js/app.js
@@ -10,18 +10,27 @@ const navList = document.getElementById("navbar__list");
 function buildingNav(){
     for(const section of allSections){
         console.log(section);
+        //grabbed the section created's data-nav word. 
         const sectionTitle = section.dataset.nav;
+        // adding a list element 
         const node = document.createElement("li"); // Create a <li> node
+        //created a link element to the list node.
         const linkNode = document.createElement("a");
+        //created a variable with the section title as its value.
         const textnode = document.createTextNode(sectionTitle); // Create a text node
+        //combined the section to the link as its child. 
         linkNode.appendChild(textnode);
+        //combined the section id value to the link pointer. ***THIS IS WHERE I NEED TO ADD THEE SMOOTH FACTOR****
         linkNode.href = `#${section.id}`;
+        //combined the entire linknode to the list node. 
         node.appendChild(linkNode);
+        //combined the list node to the navigation bar. 
         navList.appendChild(node);
     }
 }
 buildingNav(); 
 
+//I want to take the link from the nav list node and scroll smoothly to scetion.id.  
 function myFunction() {
   var elmnt = document.getElementById();
   elmnt.scrollIntoView({ behavior: 'smooth', block: 'end'});
